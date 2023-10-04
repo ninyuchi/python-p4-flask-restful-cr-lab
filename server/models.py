9ed1fa7d6a6928e5952dc399493d9ccf810418a4
Nasra Kassim <ninyuchinasra@gmail.com>
2023-10-04 18:14:33 +0300

    almost completed

--- server/models.py
+++ server/models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
+class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": self.price
+        }
